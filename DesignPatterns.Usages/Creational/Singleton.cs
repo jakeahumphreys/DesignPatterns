49fade94519eefb4ee0376b13205f8eb0ed3db30
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-15 17:58:21 +0100

    Singleton example

---
+++ DesignPatterns.Usages/Creational/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using DesignPatterns.Creational.Singleton;
+using NUnit.Framework;
+
+namespace DesignPatterns.Usages.Creational;
+
+[TestFixture]
+public class Singleton
+{
+    private Settings? _settingsOne;
+    private Settings? _settingsTwo;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _settingsOne = Settings.GetSettings();
+        _settingsTwo = Settings.GetSettings();
+    }
+
+    [Test]
+    public void Then()
+    {
+        Assert.That(_settingsTwo, Is.EqualTo(_settingsOne));
+    }
+}
---
+++ DesignPatterns/Creational/Singleton/Settings.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatterns.Creational.Singleton;
+
+public class Settings
+{
+    static Settings instance; 
+    static Dictionary<string, string> Configs = new Dictionary<string, string>();
+
+    protected Settings()
+    {
+        Configs.Add("EnableConfig", "True");
+        Configs.Add("SecondExampleConfig", "True");
+    }
+
+    public static Settings GetSettings()
+    {
+        if (instance == null)
+            instance = new Settings();
+
+        return instance;
+    }
+}
